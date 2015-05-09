8e71dc654af64db7183b1e36d50f0742f83ffa1d
Victor Bashorun <vbashorun@gmail.com>
2015-05-09 12:16:30 -0500

    Add foreign keys to user migration (commented out)

--- db/migrate/20150509102215_create_users.rb
+++ db/migrate/20150509102215_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      #t.references :user_phone, index: true, foreign_key: true
       t.references :login, index: true, foreign_key: true
       t.string :email_addr
       t.string :street
       t.string :city
       t.string :state
       t.integer :zip
+      #t.references :user_job, index: true, foreign_key: true
       t.string :first_name
       t.string :middle_initial
       t.string :last_name
