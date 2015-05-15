407d1ff1ce7e8a5e8c45ce32d0be4e0388ca726b
Victor Bashorun <vbashorun@gmail.com>
2015-05-15 16:38:46 -0500

    update migrations and added foreign keys

--- db/migrate/20150509062721_create_survey_question_options.rb
+++ db/migrate/20150509062721_create_survey_question_options.rb
@@ -1,7 +1,7 @@
 class CreateSurveyQuestionOptions < ActiveRecord::Migration
   def change
     create_table :survey_question_options do |t|
-      t.references :survey_question_id, index: true, foreign_key: true
+      t.references :survey_question, index: true, foreign_key: true
       t.integer :display_order
       t.text :text
 
--- db/migrate/20150511215452_create_user_survey_responses.rb
+++ db/migrate/20150511215452_create_user_survey_responses.rb
@@ -1,10 +1,10 @@
 class CreateUserSurveyResponses < ActiveRecord::Migration
   def change
     create_table :user_survey_responses do |t|
-      t.references :user_survey_id, index: true, foreign_key: true
-      t.references :survey_question_id, index: true, foreign_key: true
-      t.references :survey_question_option_id, index: true, foreign_key: true
-      t.references :user_survey_option_choices_id, index: true, foreign_key: true
+      t.references :user_survey, index: true, foreign_key: true
+      t.references :survey_question, index: true, foreign_key: true
+      t.references :survey_question_option, index: true, foreign_key: true
+      t.references :survey_question_options_choice, index: true, foreign_key: true
       t.text :response_text
 
       t.timestamps null: false
