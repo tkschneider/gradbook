666dd630e34154215cecc67479bbeda0bb9d3a12
Joe Sortino <joe.sortino@gmail.com>
2015-05-21 10:49:17 -0500

    cleaned code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #before_action: authenticate_login!
-
+  #before_action :authenticate_login!
   #Allows sign_up to accept more login fields besides
   before_filter :configure_devise_params, if: :devise_controller?
 
