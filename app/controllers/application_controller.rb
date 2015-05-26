81a625e59df39bc31ab3cd40982fcc6a1f1dd50a
Joe Sortino <joe.sortino@gmail.com>
2015-05-26 13:56:52 -0500

    added login and logout to topbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:first_name, :middle_initial, :last_name, :username, :type, :email, :password, :password_confirmation)
     end
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me) }
   end
+end
 end
