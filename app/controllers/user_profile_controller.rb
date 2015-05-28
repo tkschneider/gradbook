33923649f8a36400c2eb9d8e1a79433b3b5026a1
Tim Schneider <schneidert93@gmail.com>
2015-05-27 19:43:17 -0500

    Add access to company tables in user profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,6 +1,8 @@
 class UserProfileController < ApplicationController
 	def index
 	 @user = User.find(current_login.id)
+	 @company = Company.find(@user.id)
+	 @company_info = CompanyInfo.find(@company.id)
 	end
 end
 
