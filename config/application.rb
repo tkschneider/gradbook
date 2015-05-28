d3d7fa7745d7354da28d99c045ad37969e401a6f
Joe Sortino <joe.sortino@gmail.com>
2015-05-28 14:04:31 -0500

    preparing for production

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 
 require 'csv'
 
+config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
