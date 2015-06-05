3e929f89bf6a81a5cbacd6baedf1b06a122d42ae
Brad Bilter <bbbilter@noctrl.edu>
2015-06-04 22:13:38 -0500

    Fixed adding a login

--- app/models/login.rb
+++ app/models/login.rb
@@ -2,7 +2,7 @@ class Login < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   attr_accessor :logon
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:logon]
   has_one :user
